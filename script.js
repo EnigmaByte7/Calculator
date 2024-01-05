6d98a7c78be2e6508638c0dead0843e4267f7f73
EnigmaByte7 <134928189+EnigmaByte7@users.noreply.github.com>
2024-01-05 15:30:26 +0000

    feature update

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ function clear(){
 function clicked(id){
     const elem = document.getElementById(id);
     elem.style.boxShadow = 'inset 4px 4px 4px 0px rgb(0 0 0 / 31%), inset -4px -2px 8px -1px rgba(255,255,255,0.70)';
-    setTimeout(function(id){    
-        elem.style.boxShadow = '4px 4px 4px 0px rgb(0 0 0 / 31%), -4px -2px 8px -1px rgba(255,255,255,0.70)';},150);
+    setTimeout(function() {
+        elem.style.boxShadow = '4px 4px 4px 0px rgb(0 0 0 / 31%), -4px -2px 8px -1px rgba(255,255,255,0.70)';
+    }, 150);
     if (id === "C"){
         clear();
     }
@@ -34,23 +35,27 @@ function clicked(id){
             refresh('0');
         }
     }
+    else if (id === "magic") {
+        if (mood === 'day') {
+            mood = "night";
+            document.body.style.backgroundColor = 'rgb(24 24 24)';
+            document.getElementsByClassName('main')[0].style.backgroundColor = 'rgb(26 23 23)';
+            document.getElementById('display').style.backgroundColor='rgb(40 113 8)';
+            document.getElementById('display').style.boxShadow=' inset -5px -6px 9px -1px rgb(98 195 56)';
+        } else {
+            mood = 'day';
+            document.body.style.backgroundColor = '#adaeafc9';
+            document.getElementsByClassName('main')[0].style.backgroundColor = 'rgb(238, 233, 233)';
+            document.getElementById('display').style.backgroundColor='rgb(238, 233, 233)';
+            document.getElementById('display').style.boxShadow=' inset 10px 11px 9px -1px rgba(0, 0, 0, 0.425), inset -5px -6px 9px -1px rgba(255,255,255,0.70)';
+        }
+    }
     else if (
-        !(rvalue === '' && ["=", "/", "*"].includes(id)) &&
+        !(rvalue === '' && ["=", "/", "*"].includes(id)) 
         (!["/", "+", "-","*","."].includes(rvalue.slice(0,-1)) || !isNaN(Number(id)))
       ) {
         rvalue += id;
         refresh(rvalue);
       }
-    else if(id='magic'){
-        if(mood==='day'){
-            mood="night";
-            document.body.style.backgroundColor()='rgb(0 0 0)';
-            document.getElementsByClassName('main').style.backgroundColor()='rgb(0 0 0)';
-        }
-        else{
-            mood='day';
-            document.body.style.backgroundColor()='#adaeafc9';
-            document.getElementsByClassName('main').style.backgroundColor()='rgb(238, 233, 233)';
-        }
-    }
+
 }
